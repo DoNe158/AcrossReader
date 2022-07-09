4c508e673f8d5d2b175af9764a0525c9df0348c7
Dominik Jürgen Neef <dominik-juergen.neef@stud.uni-bamberg.de>
2022-07-09 20:44:09 +0200

    added validation

--- AcrossValidation.py
+++ AcrossValidation.py
@@ -1,3 +1,9 @@
+from jsonschema import validate
+import jsonschema
+import json
+import os.path
+
+
 class AcrossValidation(BaseException):
 
     @staticmethod
@@ -11,4 +17,31 @@ class AcrossValidation(BaseException):
             content = file.read()
 
         if not content.startswith('<HTML><HEAD>\n<META content="text/html; charset=UTF-8" http-equiv=Content-Type>'):
-            raise BaseException("Es handelt sich nicht um eine Across-spezifische Druckversion der Übersetzung!")
+            raise ValueError("Es handelt sich nicht um eine Across-spezifische Druckversion der Übersetzung!")
+
+    @staticmethod
+    def check_tag_file(tag_file):
+        if not tag_file.endswith(".json"):
+            raise ValueError("Fehler! Bei der ausgewählten Datei handelt es sich nicht um eine json-Datei!")
+
+    @staticmethod
+    def validate_json_schema(tag_file):
+
+        with open("tag_schema.json", "r", encoding="utf-8") as file:
+            schema = json.load(file)
+
+        try:
+            validate(instance=tag_file, schema=schema)
+            return True
+        except jsonschema.exceptions.ValidationError:
+            raise ValueError("Die ausgewählte json-Datei besitzt kein gültiges Format.")
+
+    @staticmethod
+    def validate_file_existence(file):
+        if not os.path.exists(file):
+            raise OSError("Die angegebene Datei existiert nicht!")
+
+    @staticmethod
+    def check_empty_string(string):
+        if string == '':
+            raise ValueError("Ein oder mehrere Pflichtfelder sind nicht ausgefüllt!")
