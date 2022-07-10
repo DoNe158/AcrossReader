514fb3baf1ebe6d3fde080e7b1354152258daf9f
DoNe158 <100798019+done158@users.noreply.github.com>
2022-07-10 21:47:55 +0200

    Created abstract classes for AcrossReader and GUI

---
+++ IAcrossReader.py
@@ -0,0 +1,73 @@
+import abc
+from abc import ABC, ABCMeta
+
+
+class IAcrossReader(ABC):
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def read_htm_file(self, htm_file, tag_file) -> bool:
+        """
+        Reads an htm file that is printed by Across.
+
+        :param htm_file: htm file to be read.
+        :param tag_file: tag file containing translation specific tags.
+        :return: Boolean whether the transaction was successful (True) or not (False).
+        """
+        pass
+
+    @abc.abstractmethod
+    def __save_to_tag_file__(self, tag_list, tag_file) -> bool:
+        """
+        Saves a new tag entry to an existing tag file.
+
+        :param tag_list: information of the tag (name, opening tag, closing tag, to be shown or not) in a list.
+        :param tag_file: json file where the tag is to be stored.
+        :return: Boolean whether the transaction was successful (True) or not (False).
+        """
+        pass
+
+    @staticmethod
+    def __create_new_tag_file__(tag_file_path):
+        """
+        Creates an empty json file with the basic structure (dictionary with data: []).
+
+        :param tag_file_path: path of the json file that is to be created.
+        """
+        pass
+
+    @staticmethod
+    def __show_tags__(tag_file) -> list:
+        """
+        Shows all tags with their information that are stored in the given json file that contains the tags.
+
+        :param tag_file: json file that contains the tags.
+        :return: list with tags and their specific information (name, to be shown, opening tag, closing tag.
+        """
+        pass
+
+    @abc.abstractmethod
+    def __delete_tag__(self, tag_file, tag_to_be_deleted) -> bool:
+        """
+        Deletes the given tag in the given json tag file.
+
+        :param tag_file: json file that contains the tags.
+        :param tag_to_be_deleted: tag that is to be deleted.
+        :return: Boolean whether the transaction was successful (True) or not (False)
+        """
+        pass
+
+    @classmethod
+    def __write_file_to_docx__(cls, translation_list_with_AcrossReader, document_name):
+        """
+        Takes a list with AcrossEntry stored in a AcrossReader instance and stores all entries in a docx document
+        with the given document name.
+
+        :param translation_list_with_AcrossReader: list of instances of AcrossEntry.
+        :param document_name: name of the document where the translation should be saved.
+        """
+        pass
---
+++ IGUI.py
@@ -0,0 +1,73 @@
+import abc
+from abc import ABC, ABCMeta
+
+
+class IGUI(ABC):
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def start_application(self, across_reader):
+        """
+        Starts the application.
+
+        :param across_reader: instance of the AcrossReader class.
+        """
+        pass
+
+    @abc.abstractmethod
+    def __read_htm__(self, across_reader):
+        """
+        Opens dialog for reading the htm file and the json tag file.
+
+        :param across_reader: instance of the AcrossReader class.
+        """
+        pass
+
+    @abc.abstractmethod
+    def __create_new_tag_file__(self, across_reader):
+        """
+        Opens dialog for creating a new empty tag file.
+
+        :param across_reader: instance of the AcrossReader class.
+        """
+        pass
+
+    @abc.abstractmethod
+    def __open_file__(self, tag_file, across_reader):
+        """
+        Opens the dialog for adding and deleting tags in the given tag file.
+
+        :param tag_file: json file that contains tags (or empty dictionary).
+        :param across_reader: instance of the AcrossReader class.
+
+        """
+        pass
+
+    @abc.abstractmethod
+    def __delete_tag__(self, tag_file, tag_to_be_deleted, window, across_reader):
+        """
+        Handling the deletion of the given tag in the given tag file if existing.
+
+        :param tag_file: json file that contains tags (or at least an empty dictionary in the correct format).
+        :param tag_to_be_deleted: tag that is to be deleted.
+        :param window: tkinter window.
+        :param across_reader: instance of the AcrossReader class.
+        """
+        pass
+
+    @abc.abstractmethod
+    def __save_to_tag_file__(self, tag_list, tag_file, window, across_reader):
+        """
+        Handles the storing process of a new tag to the given tag file.
+
+        :param tag_list: list containing the information of the new tag that is to be stored.
+        :param tag_file: json file that contains tags (or at least an empty dictionary in the correct format).
+        :param window: tkinter window.
+        :param across_reader: instance of the AcrossReader class.
+        """
+        pass
+
